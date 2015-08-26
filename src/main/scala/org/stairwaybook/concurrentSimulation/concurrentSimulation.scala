b78ffa01a2954c96f1c33dbcd6ceb3b19250a0b0
Haroon Khan <hkhan@healthexpense.com>
2015-08-26 13:53:59 -0700

    Complete Implementation of Concurrent Simulation

--- src/main/scala/org/stairwaybook/concurrentSimulation/concurrentSimulation.scala
+++ src/main/scala/org/stairwaybook/concurrentSimulation/concurrentSimulation.scala
@@ -4,18 +4,20 @@ import scala.actors._
 import scala.actors.Actor._
 
 case class WorkItem(time:Int, msg:Any, target:Actor)
-case class AfterDelay(delay:int, msg:Any, target:Actor)
+case class AfterDelay(delay:Int, msg:Any, target:Actor)
 case object Start
 case object Stop
+case class Ping(time:Int)
+case class Pong(time:Int, from:Actor)
 
 class Clock  extends Actor {
   private var running = false
   private var currentTime = 0
   private var agenda:List[WorkItem] = List()
   private var allSimulants:List[Actor] = List()
-  private var busySimulants:Set[Actor] = Set.empty()
+  private var busySimulants:Set[Actor] = Set()
 
-  private add(sim:Simulant) = {
+  def add(sim:Simulant) = {
     allSimulants = sim :: allSimulants
   }
 
@@ -27,11 +29,11 @@ class Clock  extends Actor {
     }
   }
 
-  def advance() = {
-    if (agenda.isEmpty && currenTime > 0 ) {
+  def advance(): Unit = {
+    if (agenda.isEmpty && currentTime > 0 ) {
       println(s"** Agenda empty. Clock exiting at time $currentTime.")
-      self ! stop
-      return
+      self ! Stop
+      return ()
     }
 
     currentTime += 1
@@ -72,6 +74,12 @@ class Clock  extends Actor {
         exit()
     }
   }
+
+  private def insert(ag:List[WorkItem], item:WorkItem):List[WorkItem] = {
+    if(ag.isEmpty || item.time < ag.head.time) item :: ag
+    else ag.head :: insert(ag.tail, item)
+  }
+
 }
 
 trait Simulant extends Actor {
@@ -118,10 +126,86 @@ class Circuit{
     clock.add(this)
 
     private var sigVal = init
-    private observers:List[Actor] = List()
+    private var observers:List[Actor] = List()
+
+    def handleSimMessage(msg:Any): Unit = msg match {
+      case SetSignal(s) =>
+        if (s!=sigVal) {
+          sigVal = s
+          signalObservers()
+        }
+    }
+
+    def signalObservers(): Unit = {
+      for (obs <- observers)
+        clock ! AfterDelay(WireDelay, SignalChanged(this, sigVal), obs)
+    }
+
+    override def simStarting() = {
+      signalObservers()
+    }
+
+    def addObserver(obs:Actor) = {
+      observers = obs :: observers
+    }
+
+    override def toString = s"Wire($name)"
   }
 
+  private object DummyWire extends Wire("dummy")
 
+  abstract class Gate(in1:Wire, in2:Wire, out:Wire) extends Simulant {
+    def computeOutput(s1:Boolean, s2:Boolean):Boolean
+    val delay:Int
+    val clock = Circuit.this.clock
+    clock.add(this)
+    in1.addObserver(this)
+    in2.addObserver(this)
+    var s1, s2 = false
+
+    override def handleSimMessage(msg: Any): Unit = msg match {
+      case SignalChanged(w, sig) =>
+        if(w == in1) s1 = sig
+        if (w == in2) s2 = sig
+        clock ! AfterDelay(delay, SetSignal(computeOutput(s1, s2)), out)
+
+    }
+  }
+
+  def orGate(in1: Wire, in2:Wire, output:Wire) = {
+    new Gate(in1, in2, output) {
+      val delay = OrGateDelay
+
+      override def computeOutput(s1: Boolean, s2: Boolean): Boolean = s1 || s2
+    }
+  }
+
+  def andGate(in1:Wire, in2:Wire, output:Wire) = {
+    new Gate(in2, in2, output) {
+      override def computeOutput(s1: Boolean, s2: Boolean): Boolean = s1 && s2
+
+      override val delay: Int = AndGateDelay
+    }
+  }
+
+  def inverter(in1:Wire, output:Wire) = {
+    new Gate(in1, DummyWire, output) {
+      override def computeOutput(s1: Boolean, s2: Boolean): Boolean = !s1
+
+      override val delay: Int = InverterDelay
+    }
+  }
+
+  def probe(wire:Wire) = new Simulant {
+    override val clock: Clock = Circuit.this.clock
+    clock.add(this)
+    wire.addObserver(this)
+    override def handleSimMessage(msg: Any): Unit = msg match {
+      case SignalChanged(w, s) => println(s"signal $w changed to $s")
+    }
+  }
+
+  def start() { clock ! Start}
 }
 
 
